f258af5ec2b97ec8180bc317751420686c47544d
mayicomein435 <rasulkhan1995@gmail.com>
2024-07-06 20:06:25 +0530

    first commit

---
+++ src/components/UserItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = ({ user, onEdit, onDelete }) => {
+  return (
+    <li>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <button onClick={() => onEdit(user)}>Edit</button>
+      <button onClick={() => onDelete(user.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default UserItem;
